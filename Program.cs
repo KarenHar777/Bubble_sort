698ae70a76173547a0508c9b8e5ec9c5b8519b9b
KarenHarutyunyan <karenvardanovich@gmail.com>
2023-01-17 23:28:42 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] num = new int[] { 4, 1, 3, 5, 2, 9, 7, 72, 6, 5 };
+
+        for (int j = 0; j < num.Length; j++)
+        {
+            Console.Write(num[j] + "\t");
+        }
+        int sort = num.Length - 1;
+
+        while (sort > 0)
+        {
+            for (int i = 0; i < num.Length - 1; i++)
+            {
+                if (num[i] > num[i + 1])
+                {
+
+                    int c = num[i];
+                    num[i] = num[i + 1];
+                    num[i + 1] = c;
+                }
+
+
+            }
+            sort--;
+        }
+
+        Console.WriteLine();
+        for (int j = 0; j < num.Length; j++)
+        {
+            Console.Write(num[j] + "\t");
+        }
+    }
+}
